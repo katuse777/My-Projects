11db99f7e7e33a1872c585d1bdae02411cd7244c
Itumeleng <seralakagiso88gmail.com>
2022-05-24 19:54:04 +0200

    I've added alot comments describing how I approach the problem and how I'm going to write out my code

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*This function should randomly RETURN rock, paper or scissors 
+  To make this random, I'll uss the Math.random function to generate 
+   a number btween 0 and 3 and I'll assign each choice a value between 0 and 3
+   and I'll use if statements to decide wht to return*/
+
+
+/*This the pseudocode for the computerPlay function
+
+  function computerPlay() {
+      generate a random number between 0 and 3, which will be 0,1 and 2
+      when I use the Math.random()*3 and I round the number to a whole number
+      I SHOULD MAKE A SEPERATE FUNCTION THAT GENERATE THE RANDOM NUMBER AND 
+      ROUNDS IT'S AND THEN CALL IT IN MY computerPlay function
+
+      now i should assign the function a return value depending on the the number that was returned
+
+      if(number is 0) return Rock
+      if(number is 1) return Paper
+      else return scissor
+  }*/
 function computerPlay() {
-    
+
 }
